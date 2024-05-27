952931396b19eebaf39459e69b4e138329de935e
Lana Petrunok <162810451+Lana-goit@users.noreply.github.com>
2024-05-27 17:55:45 +0300

    add index.html file

--- js/task-4.js
+++ js/task-4.js
@@ -10,3 +10,36 @@
 // Jamaica — 120 кредитів
 
 // Зі списку видно, що доставка можлива не скрізь. Якщо зазначена країна відсутня у списку, то функція повинна повернути рядок "Sorry, there is no delivery to your country".
+
+function getShippingCost(country) {
+  let price;
+  let message;
+  switch (country) {
+    case "China":
+      price = 100;
+      message = `Shipping to $ {country} will cost $ {price} credits`;
+      break;
+    case "Chile":
+      price = 250;
+      message = `Shipping to $ {country} will cost $ {price} credits`;
+      break;
+    case "Australia":
+      price = 170;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+    case "Jamaica":
+      price = 120;
+      message = `Shipping to ${country} will cost ${price} credits`;
+      break;
+    default:
+      return "Sorry, there is no delivery to your country";
+  }
+  return message;
+}
+
+console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
+console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
+console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
+console.log(getShippingCost("Chile")); // "Shipping to Chile will cost 250 credits"
+console.log(getShippingCost("Jamaica")); // "Shipping to Jamaica will cost 120 credits"
+console.log(getShippingCost("Sweden")); // "Sorry, there is no delivery to your country"
